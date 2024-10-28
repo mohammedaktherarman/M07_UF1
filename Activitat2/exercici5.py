e796c2cf701540a88b830113bdb9758dd20a1510
Arman Mohammed Akther <2023_arman.mohammed@iticbcn.cat>
2024-10-28 19:05:47 +0100

    docker-compose.yml fet

--- Activitat2/exercici5.py
+++
@@ -1,8 +0,0 @@
-numero1 = float(input("Primer valor:"))
-
-calcul = numero1%2
-
-if calcul==0:
-    print("El numero es parell")
-else:
-    print("El numero es senar")
--- Activitat2/exercici6.py
+++
@@ -1,9 +0,0 @@
-i = 1
-print("While")
-while i<101:
-    print(i)
-    i = i+1
-
-print("For")
-for i in range(1, 101):
-    print(i)
