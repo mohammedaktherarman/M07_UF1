d733b7c86144b36ec265e894c46bd37d2f4333f0
Arman Mohammed Akther <2023_arman.mohammed@iticbcn.cat>
2024-10-30 18:49:16 +0100

    connection.py fet

---
+++ ACTIVITAT_7/connection.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    database="postgres",
+    user="user_postgres",
+    password="pass_postgres",
+    host="localhost",
+    port="5432"
+)
+
+connection = conn.cursor()
+
+print(connection)
+
