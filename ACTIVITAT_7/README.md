c5b4d02cd4a72cab3eee918c584e8a474bea8061
Arman Mohammed Akther <2023_arman.mohammed@iticbcn.cat>
2024-11-01 10:41:50 +0100

    Activitat feta

---
+++ ACTIVITAT_7/README.md
@@ -0,0 +1,23 @@
+Sortida en el main del create.py:
+![img.png](captures/img.png)
+![img_1.png](captures/img_1.png)
+
+Sortida en el main del read.py:
+![img_2.png](captures/img_2.png)
+
+Sortida del main del update.py:
+Canvio el cognom del anterior registre:
+![img_3.png](captures/img_3.png)
+![img_4.png](captures/img_4.png)
+
+Sortida del main del delete.py:
+![img_5.png](captures/img_5.png)
+![img_6.png](captures/img_6.png)
+
+Sortida del main del create_table.py
+![img_7.png](captures/img_7.png)
+![img.png](captures/img30.png)
+
+
+
+
--- ACTIVITAT_7/update.py
+++ ACTIVITAT_7/update.py
@@ -3,19 +3,46 @@ from connection import conexio
 
 def actualitzaRegistro(user_name, user_surname, user_age, user_email, id):
     try:
-        conn = conexio()
 
+        conn = conexio()
         cursor = conn.cursor()
 
-        sql = '''update users set user_name = %s, user_surname = %s, user_age = %s, user_email = %s where user_id = %s'''
+        sql = "update users SET "
 
-        values = (user_name, user_surname, user_age, user_email, id)
+        values = []
+
+        updates = []
+
+        if user_name:
+            updates.append("user_name = %s")
+            values.append(user_name)
+
+        if user_surname:
+            updates.append("user_surname = %s")
+            values.append(user_surname)
+
+        if user_age:
+            updates.append("user_age = %s")
+            values.append(user_age)
+
+        if user_email:
+            updates.append("user_email = %s")
+            values.append(user_email)
+
+        for i in range(len(updates)):
+            sql = sql + updates[i]
+
+            if i < (len(updates) - 1):
+                sql = sql + ", "
+
+        sql = sql + " where user_id = %s"
+        values.append(id)
+
         cursor.execute(sql, values)
-
         conn.commit()
 
     except (Exception, psycopg2.Error) as error:
-        print("Error", error)
+        print("Error:", error)
 
     finally:
         conn.close()
